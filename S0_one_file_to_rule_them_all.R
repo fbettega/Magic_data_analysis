2cba541495637e69d4ef8a82aebe613bed8b7e38
fbettega <francois.bettega@gmail.com>
2024-07-25 07:30:05 +0200

    revert from gitbook and add ci in presence

--- S0_one_file_to_rule_them_all.R
+++ S0_one_file_to_rule_them_all.R
@@ -64,29 +64,49 @@ sys.source(
 rm(calledProgram)
 
 
-# calledProgram <- new.env()
-# sys.source(
-#   "6_best_deck_source.R",
-#   envir = calledProgram,
-#   toplevel.env = calledProgram
+
+fichiers_rmd <- c(
+  "rmd_files/1_new_card.Rmd",
+  "rmd_files/2_presence_archetype.Rmd",
+  "rmd_files/3_Card_win_rate_table.Rmd",
+  "rmd_files/4_matrix_WR.Rmd",
+  "rmd_files/5_Deck_analysis.Rmd",
+   "rmd_files/6_best_deck.Rmd",
+  "rmd_files/7_last_weeks_winners.Rmd"
+)
+
+
+rmarkdown::render(
+  input = "rmd_files/index.Rmd",
+  envir = new.env(),
+  output_dir = "outpout/"
+)
+
+
+# rmarkdown::render(
+#   input = "6_best_deck.Rmd",
+#   envir = new.env(),
+#   output_dir = "outpout/result"
 # )
-# rm(calledProgram)
 
 
+# rmarkdown::render('6_best_deck.knit.md')
 
 # "C:/PROGRA~1/Pandoc/pandoc" +RTS -K512m -RTS 6_best_deck.knit.md --to html4 --from markdown+autolink_bare_uris+tex_math_single_backslash --output pandoc625041177aee.html --lua-filter "C:\Users\Francois\AppData\Local\R\win-library\4.4\rmarkdown\rmarkdown\lua\pagebreak.lua" --lua-filter "C:\Users\Francois\AppData\Local\R\win-library\4.4\rmarkdown\rmarkdown\lua\latex-div.lua" --embed-resources --standalone --variable bs3=TRUE --section-divs --table-of-contents --toc-depth 3 --variable toc_float=1 --variable toc_selectors=h1,h2,h3 --variable toc_collapsed=1 --variable toc_smooth_scroll=1 --variable toc_print=1 --template "C:\Users\Francois\AppData\Local\R\win-library\4.4\rmarkdown\rmd\h\default.html" --no-highlight --variable highlightjs=1 --number-sections --variable theme=united --mathjax --variable "mathjax-url=https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML" --include-in-header "C:\Users\Francois\AppData\Local\Temp\RtmpszKuiE\rmarkdown-str62506aa4145.html" 
 # pandoc.exe: osCommitMemory: VirtualAlloc MEM_COMMIT failed to commit 12582912 bytes of memory  (error code: 1455): The paging file is too small for this operation to complete.
 # Error: pandoc document conversion failed with error 251
 
-bookdown::render_book(input = "rmd_files",
-                      output_format = bookdown::gitbook(
-                        split_by = "section"
-                      ),
-                      output_dir = "../outpout/",
-envir = new.env(),
-new_session = TRUE
-)
 
 
 
+for (fichier in fichiers_rmd) {
+  tictoc::tic(paste0(fichier))
+  rmarkdown::render(
+    input = fichier,
+    envir = new.env(),
+    output_dir = "outpout/result"
+  )
+  
+  tictoc::toc()
+}
 tictoc::toc()
