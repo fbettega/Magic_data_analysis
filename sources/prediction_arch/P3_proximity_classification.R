585c5e92c516d800370521fb73ad94eef607dd16
fbettega <francois.bettega@gmail.com>
2025-05-08 22:57:56 +0200

    massiv strange commit

--- sources/prediction_arch/P3_proximity_classification.R
+++ sources/prediction_arch/P3_proximity_classification.R
@@ -13,7 +13,7 @@ source("sources/MTG_function_base_FB/global_mtg_sources.R")
 source("sources/S2_Source_mtg_new_card.R",local = TRUE)
 
 # library("ggdendro")
-
+# format_param <- "Modern"
 # format_param <- format_date_en_cours$format_param
 
 
@@ -32,7 +32,7 @@ grouping_close_df <- function(
     
     # print(nrow(df_iteration))
     
-    already_select_arch <- rbind(
+    already_select_arch <- bind_rows(
       already_select_arch,
       data.frame(
         Archetype.x = df_iteration$Archetype.x[1],
@@ -87,7 +87,7 @@ df_export <- df_export_pre_60_filter %>%
 
 colname_deck_list <- "Mainboard"
 
-data_wide <- df_export  %>%
+data_wide_total <- df_export  %>%
   filter(Valide_deck) %>% 
   select(
     id, Archetype, Color, any_of(colname_deck_list) # ,Sideboard
@@ -113,20 +113,37 @@ data_wide <- df_export  %>%
   mutate(
     Archetype = as.factor(Archetype)
   )
-
+rm(df_export)
 
 tictoc::tic("test")
 
+
+# 1. Supprimer temporairement la colonne `id` et trouver les lignes distinctes
+data_dedup <- data_wide_total %>%
+  ungroup() %>% 
+  select(-id) %>%
+  distinct() %>%
+  mutate(dedup_key = as.character(row_number()))  # Ajoute une clé unique
+
+# 2. Rejoindre cette version dédupliquée à la dataframe de départ
+data_wide <- data_wide_total  %>%
+  left_join(data_dedup, by = colnames(data_wide)[!colnames(data_wide) %in% "id"])
+
+
+
 Dist_matrice_basale_from_wide <- as.matrix(
-  data_wide %>%
+  data_dedup %>%
     filter(Archetype != "Unknown") %>% 
-    column_to_rownames("id") %>%
-    select(-Archetype)
+    column_to_rownames("dedup_key") %>%
+    select(-Archetype) 
 )
+
+
+
 
 dist_test <- proxyC::simil(
   Dist_matrice_basale_from_wide,
-method = "ejaccard"
+  method = "ejaccard"
 )
 
 
@@ -136,28 +153,32 @@ long_dist_mat <- (
   as.data.frame() %>%
   rownames_to_column() %>%
   pivot_longer(-rowname) %>%
-  left_join(data_wide %>%
-              select(id,Archetype) %>% 
+  right_join(data_dedup %>%
+              select(dedup_key,Archetype) %>% 
               mutate(Archetype = as.character(Archetype)
-                     ),by = join_by(rowname  == id)
+                     ),by = join_by(rowname  == dedup_key)
             ) %>%
-  left_join(data_wide %>%
-              select(id,Archetype) %>% 
+  left_join(data_dedup %>%
+              select(dedup_key,Archetype) %>% 
               mutate(Archetype = as.character(Archetype)
-              ) ,by = join_by(name  == id)) %>%
+              ) ,by = join_by(name  == dedup_key)) %>%
   filter(rowname != name) # %>%   select(-c(rowname,name))
 
 
 
-
+rm(dist_test)
 df_archetype_count <- data_wide %>%
-  mutate(    Archetype = as.character(Archetype)) %>% 
+  mutate(
+    Archetype = as.character(Archetype)
+    ) %>% 
   group_by(Archetype) %>% 
   summarise(
     count = n()
   )
 
 
+rm(data_wide)
+rm(data_dedup)
 
 grouping_cards_recursive <- function(
     dist_mat_fun,
@@ -168,68 +189,72 @@ grouping_cards_recursive <- function(
     Archetype_count_fun = df_archetype_count,
     n = 1
 ){
-  
-  
-  # print(n)
+
+
+  print(n)
 
   if(n > 50) {system("shutdown -s")}
 
   summarise_dist_jaccard <- dist_mat_fun %>%
-    group_by(Archetype.x ,Archetype.y) %>% 
+    group_by(Archetype.x ,Archetype.y) %>%
     summarize(
       count = n(),
       Q2 = quantile(value ,0.5),
       .groups = "drop"
-    ) %>% 
+    ) %>%
     left_join(
-      dist_mat_fun %>% 
-                filter(Archetype.x == Archetype.y) %>% 
-                select(-Archetype.y) %>% 
-                group_by(Archetype.x) %>% 
+      dist_mat_fun %>%
+                filter(Archetype.x == Archetype.y) %>%
+                select(-Archetype.y) %>%
+                group_by(Archetype.x) %>%
                 summarize(
                   Q3 = quantile(value ,0.75),
                   .groups = "drop"
-                ) %>% 
-                rename_all(~paste0("self_",.)) %>% 
+                ) %>%
+                rename_all(~paste0("self_",.)) %>%
                 rename(Archetype = self_Archetype.x) ,
               by = join_by(Archetype.x == Archetype)
-      ) %>% 
+      ) %>%
     mutate(
       Q3_compare_Median = self_Q3 - Q2
-    ) %>% 
-    filter( Archetype.x != Archetype.y   ) %>% 
-    filter(Q3_compare_Median >= 0) %>% 
+    ) %>%
+    filter( Archetype.x != Archetype.y   ) %>%
+    filter(Q3_compare_Median >= 0) %>%
     arrange(desc(Q3_compare_Median))
-  
-  
-  
+
+
+
   if(nrow(summarise_dist_jaccard) > 0){
   print(paste0("Remaining archetype to proximity join : ",nrow(summarise_dist_jaccard)))
-    
-  archetype_to_group <- grouping_close_df(summarise_dist_jaccard) %>% 
-    left_join(Archetype_count_fun,by = join_by(Archetype.x == Archetype)) %>% 
-    rename(count.x = count) %>%   
-    left_join(Archetype_count_fun,by = join_by(Archetype.y == Archetype)) %>% 
-    rename(count.y = count) %>% 
+
+  archetype_to_group <- grouping_close_df(summarise_dist_jaccard) %>%
+    left_join(Archetype_count_fun,by = join_by(Archetype.x == Archetype)) %>%
+    rename(count.x = count) %>%
+    left_join(Archetype_count_fun,by = join_by(Archetype.y == Archetype)) %>%
+    rename(count.y = count) %>%
     mutate(Archetype_name = ifelse(count.x < count.y,
                                    Archetype.y,
                                    Archetype.x
-    )) %>% 
-    select(-starts_with("count")) %>% 
-    pivot_longer(-Archetype_name) %>% 
-    select(-name) %>% 
+    )) %>%
+    select(-starts_with("count")) %>%
+    pivot_longer(-Archetype_name) %>%
+    select(-name) %>%
     filter(Archetype_name != value)
-  
-  grouping_df_res <- rbind(grouping_df_base_fun,archetype_to_group)
-  
-  
-  
-  dist_mat_fun_res <- dist_mat_fun %>% 
+
+  grouping_df_res <- bind_rows(grouping_df_base_fun,archetype_to_group)
+
+
+
+  dist_mat_fun_res <- dist_mat_fun %>%
     filter(
       Archetype.x %notin% grouping_df_res$value,
       Archetype.y %notin% grouping_df_res$value
            )
-    
+  # Nettoyage mémoire explicite
+  rm(dist_mat_fun)
+  rm(summarise_dist_jaccard)
+  gc(verbose = FALSE)
+
     res <- grouping_cards_recursive(
       dist_mat_fun = dist_mat_fun_res ,
       grouping_df_base_fun = grouping_df_res,
@@ -237,22 +262,23 @@ grouping_cards_recursive <- function(
       n = n + 1
       )
    } else{
-    res <- grouping_df_base_fun 
-    
+    res <- grouping_df_base_fun
+
   }
-  
-  
-return(res)  
-  
-  
+
+
+return(res)
+
+
 }
 
+gc()
 
 resulting_distance_mat_with_all_group <- grouping_cards_recursive(
   dist_mat_fun = long_dist_mat
 )  %>% 
   rename(Archetype_proximity = Archetype_name )
-
+gc()
 
 
 res_proximity_joins <- df_export_pre_60_filter %>% 
@@ -273,15 +299,6 @@ res_proximity_joins <- df_export_pre_60_filter %>%
   mutate(
     Archetype_count = n()) %>% 
   ungroup() 
-
-
-
-
-
-
-
-
-
 
 
 
